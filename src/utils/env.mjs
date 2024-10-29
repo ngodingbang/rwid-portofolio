db6fd672f936abdc851d4de60e0696864481a943
Ngoding Bang <ngodingbang@gmail.com>
2024-10-29 16:38:58 +0700

    refactor: remove unused env config

--- src/utils/env.mjs
+++ src/utils/env.mjs
@@ -6,35 +6,6 @@ export const envSchema = z.object({
     .transform((value) => value || "production"),
   APP_URL: z.string().transform((value) => value || "https://localhost:3000"),
   NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: z.string(),
-
-  nextImageExportOptimizer_imageFolderPath: z
-    .string()
-    .optional()
-    .transform((value) => value || "public/images"),
-  nextImageExportOptimizer_exportFolderPath: z
-    .string()
-    .optional()
-    .transform((value) => value || "out"),
-  nextImageExportOptimizer_quality: z
-    .string()
-    .optional()
-    .transform((value) => value || "75"),
-  nextImageExportOptimizer_storePicturesInWEBP: z
-    .string()
-    .optional()
-    .transform((value) => value || "true"),
-  nextImageExportOptimizer_exportFolderName: z
-    .string()
-    .optional()
-    .transform((value) => value || "nextImageExportOptimizer"),
-  nextImageExportOptimizer_generateAndUseBlurImages: z
-    .string()
-    .optional()
-    .transform((value) => value || "true"),
-  nextImageExportOptimizer_remoteImageCacheTTL: z
-    .string()
-    .optional()
-    .transform((value) => value || "0"),
 });
 
 // Prevent default process.env.NODE_ENV to be parsed on next.config.mjs
